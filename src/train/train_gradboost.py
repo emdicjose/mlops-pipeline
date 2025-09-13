2dc230945675237428e99c6b08abdfb52c048d36
José <emdicjose@gmail.com>
2025-09-13 15:47:06 +0200

    finish train scripts

--- src/train/train_gradboost.py
+++ src/train/train_gradboost.py
@@ -2,13 +2,14 @@ import os
 import mlflow
 import mlflow.sklearn
 from mlflow.models.signature import infer_signature
-
 from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
+
 
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import ElasticNet
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.ensemble import GradientBoostingRegressor
+
 import numpy as np
 
 # set tracking uri
@@ -16,32 +17,41 @@ os.environ['MLFLOW_TRACKING_URI'] = "http://127.0.0.1:5000"
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 # set model name
-model_name = "en"
-model_type = "ElasticNet"
+model_name = "gb"
+model_type = "GradientBoosting"
 
-# set experiment name 
 mlflow.set_experiment("diabetes-example")
 
 # Load dataset
 X, y = load_diabetes(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+
 # Define search space
 space = {
-    "alpha": hp.uniform("alpha", 0.01, 1.0),
-    "l1_ratio": hp.uniform("l1_ratio", 0.0, 1.0)
+    "n_estimators": hp.quniform("n_estimators", 50, 200, 10),  # number of trees
+    "max_depth": hp.quniform("max_depth", 2, 10, 1),           # max tree depth
+    "learning_rate": hp.uniform("learning_rate", 0.01, 0.3),   # step size shrinkage
+    "subsample": hp.uniform("subsample", 0.6, 1.0),            # row sampling
 }
 
 # Objective function
 def objective(params):
     with mlflow.start_run(
-        run_name = f"{model_name}_a{params['alpha']:.2f}_l1_{params['l1_ratio']:.2f}"
+        run_name = f"{model_name}_n{params['n_estimators']}_md{params['max_depth']}"
     ):
-        model = ElasticNet(**params, random_state=42)
-        
+        model = GradientBoostingRegressor(
+            n_estimators=int(params['n_estimators']),
+            max_depth=int(params['max_depth']),
+            learning_rate=params['learning_rate'],
+            subsample=params['subsample'],
+            random_state=42
+        )
+
+
         model.fit(X_train, y_train)
         preds = model.predict(X_test)
-        
+
         # Metrics
         rmse = np.sqrt(mean_squared_error(y_test, preds))
         mae = mean_absolute_error(y_test, preds)
@@ -49,18 +59,22 @@ def objective(params):
 
         # Log params
         mlflow.log_params(params)
-        mlflow.log_param("model_type", model_type)
-
+        mlflow.log_param("model_type",model_type)
+        
         # Log metrics
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("mae", mae)
         mlflow.log_metric("r2", r2)
-        
-        # Log model 
+
+        # Log model
         signature = infer_signature(X_train, model.predict(X_train))
         mlflow.sklearn.log_model(model, name="model", signature=signature, input_example=X_train[:5])
 
+        print(f"[XGBoost] RMSE={rmse:.3f}, MAE={mae:.3f}, R2={r2:.3f}")
+    
     return {"loss": rmse, "status": STATUS_OK}
+
+
 
 trials = Trials()
 
--- src/train/train_randomforest.py
+++ src/train/train_randomforest.py
@@ -3,6 +3,8 @@ import mlflow
 import mlflow.sklearn
 from mlflow.models.signature import infer_signature
 
+from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
+
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
@@ -14,7 +16,9 @@ os.environ['MLFLOW_TRACKING_URI'] = "http://127.0.0.1:5000"
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 # set model name
-model_name = "RandomForest"
+model_name = "rf"
+model_type = "RandomForest"
+
 
 mlflow.set_experiment("diabetes-example")
 
@@ -22,27 +26,61 @@ mlflow.set_experiment("diabetes-example")
 X, y = load_diabetes(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Random hyperparameters
-n_estimators = np.random.randint(50, 200)
-max_depth = np.random.randint(2, 10)
+# Define search space
+space = {
+    "n_estimators": hp.quniform("n_estimators", 50, 200, 10),
+    "max_depth": hp.quniform("max_depth", 2, 10, 1),
+    "max_features": hp.choice("max_features", ["sqrt", "log2"]),
+    "min_samples_split": hp.quniform("min_samples_split", 2, 5, 1),
+    "min_samples_leaf": hp.quniform("min_samples_leaf", 1, 3, 1)
+}
 
-with mlflow.start_run():
-    model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
-    model.fit(X_train, y_train)
-    preds = model.predict(X_test)
+# Objective function
+def objective(params):
+    with mlflow.start_run(
+        run_name = f"{model_name}_n{params['n_estimators']:.2f}_d{params['max_depth']:.2f}_mf_{params['max_features']}"
+        ):
+        
+        model = RandomForestRegressor(
+            n_estimators=int(params['n_estimators']),
+            max_depth=int(params['max_depth']),
+            max_features=params['max_features'],
+            min_samples_split=int(params['min_samples_split']),
+            min_samples_leaf=int(params['min_samples_leaf']),
+            random_state=42)
+        
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
 
-    # Metrics
-    rmse = mean_squared_error(y_test, preds, squared=False)
-    mae = mean_absolute_error(y_test, preds)
-    r2 = r2_score(y_test, preds)
+        # Metrics
+        rmse = np.sqrt(mean_squared_error(y_test, preds))
+        mae = mean_absolute_error(y_test, preds)
+        r2 = r2_score(y_test, preds)
 
-    # Log params, metrics, model
-    mlflow.log_param("n_estimators", n_estimators)
-    mlflow.log_param("max_depth", max_depth)
-    mlflow.log_param("model_type", "RandomForest")
-    mlflow.log_metric("rmse", rmse)
-    mlflow.log_metric("mae", mae)
-    mlflow.log_metric("r2", r2)
-    mlflow.sklearn.log_model(model, artifact_path="model")
+        # Log params
+        mlflow.log_params(params)
+        mlflow.log_param("model_type", model_type)
 
-    print(f"[RandomForest] RMSE={rmse:.3f}, MAE={mae:.3f}, R2={r2:.3f}")
+        
+        # Log metrics
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
+        
+        # Log model
+        signature = infer_signature(X_train, model.predict(X_train))
+        mlflow.sklearn.log_model(model, name="model", signature=signature, input_example=X_train[:5])
+
+    return {"loss": rmse, "status": STATUS_OK}
+
+trials = Trials()
+
+best = fmin(
+    fn=objective,
+    space=space,
+    algo=tpe.suggest,
+    max_evals=5,  # number of hyperparameter combinations to try
+    trials=trials
+)
+
+print("Best hyperparameters:", best)
